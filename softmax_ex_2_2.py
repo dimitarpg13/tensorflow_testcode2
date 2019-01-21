47595d1d0d5ee178f8b5e10e7d569c712e7d83a3
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2019-01-21 08:53:08 -0500

    first commit

---
+++ softmax_ex_2_2.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+DATA_DIR = '/tmp/data'
+NUM_STEPS = 1000
+MINIBATCH_SIZE = 100
+
+data = input_data.read_data_sets(DATA_DIR, one_hot=True)
+x = tf.placeholder)tf.float32, [None, 784])
+W = tf.Variable(tf.zeros([784, 10]))
+
+
---
+++ softmax_grad_descent_opt.py
@@ -0,0 +1,49 @@
+import tensorflow as tf
+
+# import MNIST data
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+
+learning_rate = 0.01
+training_epochs = 10
+batch_size = 100
+display_step = 1
+
+#tf Graph input
+x = tf.placeholder(tf.float32, [None, 784]) # mnist data image of shape 28*28=784
+y = tf.placeholder(tf.float32, [None, 10]) # 0-9 digits recognition => 10 classes
+
+# set model weights
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+
+# minimize error using cross entropy 
+cost = tf.reduce_mean(-tf.reduce_sum(y*tf.log(pred), reduction_indices=1))
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+
+# start training
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+
+    # training cycle
+    for epoch in range(training_epochs):
+        avg_cost = 0.
+        total_batch = int(mnist.train.num_examples/batch_size)
+        # loop over all batches
+        for i in range(total_batch):
+            batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+            # fit training using batch data
+            _, c = sess.run([optimizer, cost], feed_dict={x: batch_xs, 
+                y: batch_ys})
+
+            # print(__w)
+
+            # compute average loss
+            avg_cost += c / total_batch
+        # display logs per epoch step
+        if (epoch+1) % display_step == 0:
+            # print(sess.run(W))
+            print ("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(avg_cost))
+
+    print ("Optimization Finished!")
